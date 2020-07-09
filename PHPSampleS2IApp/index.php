2d9db272e2f73359fbfed3010edb2f851645327c
TheAppService <web.par@t-online.de>
2020-07-09 16:02:51 +0200

    Update index.php

--- PHPSampleS2IApp/index.php
+++ PHPSampleS2IApp/index.php
@@ -6,6 +6,6 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
-echo $twig->render('main.html.twig', ['name' => 'This is a hello world Test Application, running on', 
+echo $twig->render('main.html.twig', ['name' => 'This is a hello world Test Application', 
     'osV' => 'OpenShift']);
 ?>
