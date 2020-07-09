02dc95a3be4530c56e9dc8349779f017ab5a8531
TheAppService <web.par@t-online.de>
2020-07-09 15:42:39 +0200

    Add files via upload

---
+++ PHPSampleS2IApp/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'vendor/autoload.php'
+  
+echo "Hello World, this is a test application.";
+echo "It should compile with composer automatically";
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+echo $twig->render('main.html.twig', ['name' => 'John Doe', 
+    'occupation' => 'gardener']);
+
+
+
+
+?>
