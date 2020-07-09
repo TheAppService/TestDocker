f5cf80ae52487cae5bdf2b46da84b40f79bbc7a5
TheAppService <web.par@t-online.de>
2020-07-09 16:01:52 +0200

    Update index.php

--- PHPSampleS2IApp/index.php
+++ PHPSampleS2IApp/index.php
@@ -1,19 +1,11 @@
+
 <?php
 require_once 'vendor/autoload.php';
   
-echo "Hello World, this is a test application.";
-echo "It should compile with composer automatically";
-
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
-
-echo $twig->render('main.html.twig', ['name' => 'John Doe', 
-    'occupation' => 'gardener']);
-
-
-
-
+echo $twig->render('main.html.twig', ['name' => 'This is a hello world Test Application, running on', 
+    'osV' => 'OpenShift']);
 ?>
